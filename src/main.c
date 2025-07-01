026bfb576f6cba6b310fabbad75f35348734378b
torbatti <torbattii@gmail.com>
2025-07-01 14:35:39 +0330

    add: call zig function from a c_api inside c code

---
+++ src/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <root_c.h>
+
+int main(void){
+    int num = add(1,3);
+    printf("%d\n",num);
+    printf("Hello, World!\n");
+    return 0;
+}
---
+++ src/root_c.zig
@@ -0,0 +1,3 @@
+export fn add(num_1: c_int, num_2: c_int) c_int {
+    return num_1 + num_2;
+}
